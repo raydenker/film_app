0fa1de145502de9a2a31348abb7a017af63bcf5f
raydenker <raydenkerlarin@gmail.com>
2022-06-18 20:48:55 +0300

    Updates

---
+++ precache-manifest.3dfa7510963bb22afb1932b9e83b5986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a188202e230cdcad7eb",
+    "url": "/static/css/main.cc38a22e.chunk.css"
+  },
+  {
+    "revision": "5a188202e230cdcad7eb",
+    "url": "/static/js/main.5a188202.chunk.js"
+  },
+  {
+    "revision": "a27bcabbdc9ca55fd22e",
+    "url": "/static/js/1.a27bcabb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65870d4a7c5f9e7dee929958cea011c4",
+    "url": "/index.html"
+  }
+];
